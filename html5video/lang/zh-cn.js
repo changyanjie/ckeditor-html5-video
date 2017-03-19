d9a4c187d2707285b42db81d9e3456492ef2e531
Yanjie.Chang <yanjie.chang@ele.me>
2017-03-19 12:57:39 +0800

    up

---
+++ html5video/lang/zh-cn.js
@@ -0,0 +1,15 @@
+CKEDITOR.plugins.setLang( 'html5video', 'zh-cn', {
+    button: '插入视频',
+    title: 'HTML5视频',
+    infoLabel: '视频信息',
+    allowed: '支持格式: MP4',
+    urlMissing: '视频地址丢失.',
+    videoProperties: '视频属性',
+    upload: '上传',
+    btnUpload: '上传到服务器',
+    advanced: '高级',
+    autoplay: '自动播放',
+    yes: '是',
+    no: '否',
+	responsive: '页面宽度'
+} );
